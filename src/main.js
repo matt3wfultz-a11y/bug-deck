4d5825cfd477451b30fd0f7582b690d22897f501
Cecilia Amador <ceciliaamador@Cecilias-MBP.lan>
2026-04-11 14:02:51 -0500

    fix: add imports to main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,31 @@
+import Creature from './entities/Creature.js';
+import BattleSystem from './systems/BattleSystem.js';
+import GameState from './systems/GameState.js';
+import MenuScene from './scenes/MenuScene.js';
+import BattleScene from './scenes/BattleScene.js';
+import CaptureScene from './scenes/CaptureScene.js';
+import FarmScene from './scenes/FarmScene.js';
+
+// Expose to window for console testing
+window.Creature = Creature;
+window.BattleSystem = BattleSystem;
+window.GameState = GameState;
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: '#1a1a2e',
+  parent: document.body,
+  scene: [MenuScene, BattleScene, CaptureScene, FarmScene],
+};
+
+window.main = function () {
+  return new Phaser.Game(config);
+};
+
+window.main();
+
 import MenuScene    from './scenes/MenuScene.js';
 import BattleScene  from './scenes/BattleScene.js';
 import CaptureScene from './scenes/CaptureScene.js';
